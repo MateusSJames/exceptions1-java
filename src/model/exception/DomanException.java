9e61d04cecf5bf7f505c298cd512d04ad97ec752
Mateus Aristides <mateusaristides23@gmail.com>
2020-05-20 19:22:20 -0300

    Solucao boa

---
+++ src/model/exception/DomanException.java
@@ -0,0 +1,9 @@
+package model.exception;
+
+public class DomanException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DomanException(String msg) {
+		super(msg);
+	}
+}
